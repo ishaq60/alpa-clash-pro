aa6413727ec84c97c2b84380f8e3b8e91b0a1d33
ishaq60 <ishaqahammad60@gmail.com>
2024-02-14 22:08:05 +0600

    home page and playground are done

--- script/Alpha.js
+++ script/Alpha.js
@@ -7,7 +7,19 @@
 // // console.log(PlaygroundSection.classList)
 // PlaygroundSection.classList.remove('hidden')
 // }
+function continueGame(){
+    // step-1 :genarate a random alpahabet
+    const alpahabet=getArandomAlapabet();
+    console.log('the random value',alpahabet);
+    // set randomly genarated alpahbet to screen(show it)
+    const currentAlphabetElement =document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText=alpahabet;
+    setBackgroundColorById(alpahabet);
+}
+// set background color
+
 function play(){
     hidelementById('home-screen');
     showElementById('play-ground');
+   continueGame()
 }
--- script/utility.js
+++ script/utility.js
@@ -6,3 +6,24 @@ function showElementById(elementId){
     const element =document.getElementById(elementId)
     element.classList.remove('hidden')
 }
+
+
+function setBackgroundColorById(elementId){
+  const element=document.getElementById(elementId);
+  element.classList.add('bg-amber-900');
+}
+
+function getArandomAlapabet(){
+  //get or create an alphabets array
+    const alpabetstring='abbtuihrrhrhihhuerhoa';
+    const alpahabets=alpabetstring.split('');
+    console.log(alpahabets);
+    //get a random index between 0-25
+    const randomNumber =Math.random()*25;
+    const index=Math.round(randomNumber)
+    console.log(index);
+    const alpahabet=alpahabets[index];
+    console.log(alpahabet);
+    return alpahabet;
+
+}
